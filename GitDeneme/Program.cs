2c63a026627001a583d8ff291b67d7278a961311
Mustafa SOYSAL (BIlgeAdam) <mustafa.soysal@bilgeadam.com>
2019-04-15 21:36:20 +0300

    first commit

--- GitDeneme/Program.cs
+++ GitDeneme/Program.cs
@@ -7,6 +7,7 @@ namespace GitDeneme
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("First Commit");
             Console.ReadKey();
         }
     }
